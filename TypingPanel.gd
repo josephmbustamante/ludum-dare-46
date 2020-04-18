65c11dabf6163b10c635dac20f5e629d19fb0647
Jason Porritt <jason.porritt@atomicobject.com>
2020-04-18 12:19:13 -0400

    Adds feedback on text entry.

--- TypingPanel.gd
+++ TypingPanel.gd
@@ -6,6 +6,9 @@ onready var input_text = $InputText
 
 var text_to_type: String = ""
 var current_character_index: int = -1
+var matched: bool = false
+
+signal input_finished(matched)
 
 
 func _ready() -> void:
@@ -16,12 +19,24 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey and not event.is_pressed():
 		var a: InputEventKey = event
-		var key_press_string = OS.get_scancode_string(a.scancode)
-		var next_character = text_to_type.substr(current_character_index, 1)
-		if key_press_string == next_character:
-			print("CORRECTLY TYPED %s" % key_press_string)
-			current_character_index += 1
+		var key_code = OS.get_scancode_string(event.scancode)
+
+		if key_code == "Enter" or key_code == "Escape":
+			print("DONE")
+			emit_signal("input_finished", matched)
 		else:
-			print("INCORRECTLY TYPED %s instead of %s" % [key_press_string, next_character])
+			var raw_data = PoolByteArray([a.unicode])
+			var key_press_string = raw_data.get_string_from_utf8()
+			var next_character = text_to_type.substr(current_character_index, 1)
+			if key_press_string == next_character:
+				
+				if current_character_index == text_to_type.length() - 1:
+					input_text.parse_bbcode("[color=green]" + text_to_type + "[/color]")
+					matched = true
+				else:
+					current_character_index += 1
+					input_text.parse_bbcode("[color=black]" + text_to_type.substr(0, current_character_index) + "[/color]" + text_to_type.substr(current_character_index, -1))
+			else:
+				print("INCORRECTLY TYPED %s instead of %s" % [key_press_string, next_character])
 
 	get_tree().set_input_as_handled()
