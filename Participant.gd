10b6642156e675bb699274744eb61a274932aed1
Joe Bustamante <joe.bustamante@atomicobject.com>
2020-04-18 13:05:58 -0400

    Add a function to handle participant engagement level changing

--- Participant.gd
+++ Participant.gd
@@ -2,8 +2,16 @@ extends Node2D
 class_name Participant
 
 
+signal participant_engagement_level_changed(engagement_level)
+
+
 export (String) var first_name = ""
 export (String) var last_name = ""
-export (int, 0, 100) var engagement_level = 100
+export (int, 0, 100) var engagement_level = 100 setget set_engagement_level
+
+
+func set_engagement_level(new_engagement_level: int):
+	engagement_level = clamp(new_engagement_level, 0, 100)
+	emit_signal("participant_engagement_level_changed", engagement_level)
 
 
