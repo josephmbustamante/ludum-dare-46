e6ab5a0cbe8d75c1c9b97d8c65445345c033b6f9
Joe Bustamante <joe.bustamante@atomicobject.com>
2020-04-18 08:43:23 -0400

    Add a popup to show what the player has selected and add code to switch that using tab

--- Main.gd
+++ Main.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 
+onready var selected_object_notifier = $SelectedObjectNotifier
+
 
 var interactable_objects: Array = []
 var currently_selected_object_index: int = -1
@@ -18,21 +20,35 @@ func _unhandled_input(event: InputEvent) -> void:
 				currently_selected_object_index += 1
 			else:
 				currently_selected_object_index = 0
+			show_selected_object_notifier(interactable_objects[currently_selected_object_index].global_position)
+
 	if event.is_action_released("ui_focus_prev"):
 		if interactable_objects.size() > 0:
 			if currently_selected_object_index == 0:
 				currently_selected_object_index = interactable_objects.size() - 1
 			else:
 				currently_selected_object_index -= 1
+			show_selected_object_notifier(interactable_objects[currently_selected_object_index].global_position)
 
 
 func _on_Player_player_can_interact_with_object(object) -> void:
 	interactable_objects.append(object)
 	if interactable_objects.size() == 1:
 		currently_selected_object_index = 0
+		show_selected_object_notifier(interactable_objects[currently_selected_object_index].global_position)
 
 
 func _on_Player_player_cannot_interact_with_object(object) -> void:
 	interactable_objects.remove(object)
 	if interactable_objects.size() == 0:
 		currently_selected_object_index = -1
+		hide_selected_object_notifier()
+
+
+func show_selected_object_notifier(location: Vector2):
+	selected_object_notifier.show()
+	selected_object_notifier.global_position = location + Vector2(0, -20)
+
+
+func hide_selected_object_notifier():
+	selected_object_notifier.hide()
