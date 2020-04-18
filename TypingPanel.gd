5a209118786a10e681789f300a093e6367fa5807
Jason Porritt <jason.porritt@atomicobject.com>
2020-04-18 12:32:37 -0400

    Show underline to make progress more clear.

--- TypingPanel.gd
+++ TypingPanel.gd
@@ -24,19 +24,20 @@ func _input(event: InputEvent) -> void:
 		if key_code == "Enter" or key_code == "Escape":
 			print("DONE")
 			emit_signal("input_finished", matched)
-		else:
+		elif current_character_index < text_to_type.length():
 			var raw_data = PoolByteArray([a.unicode])
 			var key_press_string = raw_data.get_string_from_utf8()
 			var next_character = text_to_type.substr(current_character_index, 1)
+
 			if key_press_string == next_character:
-				
+				current_character_index += 1
 				if current_character_index == text_to_type.length() - 1:
 					input_text.parse_bbcode("[color=green]" + text_to_type + "[/color]")
 					matched = true
 				else:
-					current_character_index += 1
-					input_text.parse_bbcode("[color=black]" + text_to_type.substr(0, current_character_index) + "[/color]" + text_to_type.substr(current_character_index, -1))
+					input_text.parse_bbcode("[u][color=black]" + text_to_type.substr(0, current_character_index) + "[/color][/u]" + text_to_type.substr(current_character_index, -1))
 			else:
 				print("INCORRECTLY TYPED %s instead of %s" % [key_press_string, next_character])
+				input_text.parse_bbcode("[u][color=black]" + text_to_type.substr(0, current_character_index) + "[/color][color=red]" + text_to_type.substr(current_character_index, 1) + "[/color][/u]" + text_to_type.substr(current_character_index + 1, -1))
 
 	get_tree().set_input_as_handled()
