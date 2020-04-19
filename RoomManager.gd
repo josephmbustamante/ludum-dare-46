d27f81ac83409240a51789432480dc3e4c1aa7a7
Joe Bustamante <joe.bustamante@atomicobject.com>
2020-04-18 21:48:12 -0400

    Add basic room manager and display, plus a reusable progress bar

---
+++ RoomManager.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+
+signal wifi_level_changed(new_wifi_level)
+
+
+export (int, 0, 100) var wifi_level: int = 100 setget set_wifi_level
+
+
+onready var room_manager_tick_timer = $RoomManagerTickTimer
+
+
+func _ready() -> void:
+	room_manager_tick_timer.start()
+
+
+func set_wifi_level(new_wifi_level):
+	var potential_new_wifi_level = clamp(new_wifi_level, 0 ,100)
+	if potential_new_wifi_level != wifi_level:
+		wifi_level = potential_new_wifi_level
+		emit_signal("wifi_level_changed", wifi_level)
+
+
+func _on_RoomManagerTickTimer_timeout() -> void:
+	wifi_level -= 1
