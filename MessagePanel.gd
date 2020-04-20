94559b30863b7602c509822f3d1f001fd7d9310d
Joe Bustamante <joe.bustamante@atomicobject.com>
2020-04-20 16:15:12 -0400

    Fix bug where no meeting message wouldn't go away

--- MessagePanel.gd
+++ MessagePanel.gd
@@ -2,14 +2,20 @@ extends Node2D
 class_name MessagePanel
 
 onready var message_label = $MessageText
+onready var animation_player = $AnimationPlayer
+
+
+var is_open: bool = false
+
 
 func show_message(message) -> void:
 	print("GOT show_message call")
 	message_label.text = message
-	visible = true
+	animation_player.play("show")
+	is_open = true
 
 func _input(event: InputEvent) -> void:
-	if not visible:
+	if not is_open:
 		return
 
 	if event is InputEventKey and not event.is_pressed():
@@ -19,4 +25,11 @@ func _input(event: InputEvent) -> void:
 		# If we press Enter or Escape, exit, regardless of progress or session
 		if key_code == "Enter" or key_code == "Escape":
 			get_tree().set_input_as_handled()
-			visible = false
+			animation_player.stop()
+			modulate = Color(1, 1, 1, 0)
+			is_open = false
+
+
+func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
+	if anim_name == "show":
+		is_open = false
