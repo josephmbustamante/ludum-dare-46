c12d9262f3f5d8e92285bf77885e48eb2475d262
Joe Bustamante <joe.bustamante@atomicobject.com>
2020-04-20 18:58:48 -0400

    Tweak ratings at the end to more accurately reflect possible scores

--- GameOverPanel.gd
+++ GameOverPanel.gd
@@ -11,28 +11,28 @@ func update_game_over_screen(final_score: int):
 
 
 func set_rating_text(rating: int):
-	if rating >= 800:
+	if rating >= 900:
 		rating_value.text = "MASTERFUL"
 		rating_value.set("custom_colors/font_color", Color.purple)
-	elif rating >= 700:
+	elif rating >= 800:
 		rating_value.text = "EXCELLENT"
 		rating_value.set("custom_colors/font_color", Color.blue)
-	elif rating >= 600:
+	elif rating >= 700:
 		rating_value.text = "VERY GOOD"
 		rating_value.set("custom_colors/font_color", Color.forestgreen)
-	elif rating >= 500:
+	elif rating >= 600:
 		rating_value.text = "GOOD"
 		rating_value.set("custom_colors/font_color", Color.lime)
-	elif rating >= 400:
+	elif rating >= 500:
 		rating_value.text = "AVERAGE"
 		rating_value.set("custom_colors/font_color", Color.yellow)
-	elif rating >= 300:
+	elif rating >= 400:
 		rating_value.text = "OKAY"
 		rating_value.set("custom_colors/font_color", Color.orange)
-	elif rating >= 200:
+	elif rating >= 300:
 		rating_value.text = "POOR"
 		rating_value.set("custom_colors/font_color", Color.orangered)
-	elif rating >= 100:
+	elif rating >= 200:
 		rating_value.text = "VERY POOR"
 		rating_value.set("custom_colors/font_color", Color.red)
 	else:
