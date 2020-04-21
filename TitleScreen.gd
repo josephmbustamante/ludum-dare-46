0c6a23dba8739b275e77cf63fc6815a8a5df007a
Joe Bustamante <joe.bustamante@atomicobject.com>
2020-04-20 22:09:04 -0400

    Add an actual updated title screen and increase font size

--- TitleScreen.gd
+++ TitleScreen.gd
@@ -3,6 +3,9 @@ extends Node2D
 
 onready var tutorials = $Tutorials
 onready var tutorial_button = $ShowTutorialButton
+onready var title = $Title
+onready var title_background = $TitleBackground
+onready var background_image = $BackgroundImage
 
 
 func _on_PlayButton_pressed() -> void:
@@ -12,7 +15,13 @@ func _on_PlayButton_pressed() -> void:
 func _on_ShowTutorialButton_pressed() -> void:
 	if tutorials.visible:
 		tutorials.hide()
+		title.show()
+		title_background.show()
+		background_image.modulate = Color(0.639216, 0.686275, 0.933333, 0.196078)
 		tutorial_button.text = "SHOW TUTORIALS"
 	else:
 		tutorials.show()
+		title.hide()
+		title_background.hide()
+		background_image.modulate = Color(1, 1, 1, 1)
 		tutorial_button.text = "HIDE TUTORIALS"
