f709bf15140eb7569a0059865f8a5db5aed0b690
Joe Bustamante <joe.bustamante@atomicobject.com>
2020-04-20 22:22:48 -0400

    Finish fixing title screen and tutorial fonts

--- TitleScreen.gd
+++ TitleScreen.gd
@@ -6,6 +6,7 @@ onready var tutorial_button = $ShowTutorialButton
 onready var title = $Title
 onready var title_background = $TitleBackground
 onready var background_image = $BackgroundImage
+onready var play_button = $PlayButton
 
 
 func _on_PlayButton_pressed() -> void:
@@ -17,11 +18,13 @@ func _on_ShowTutorialButton_pressed() -> void:
 		tutorials.hide()
 		title.show()
 		title_background.show()
+		play_button.show()
 		background_image.modulate = Color(0.639216, 0.686275, 0.933333, 0.196078)
 		tutorial_button.text = "SHOW TUTORIALS"
 	else:
 		tutorials.show()
 		title.hide()
 		title_background.hide()
+		play_button.hide()
 		background_image.modulate = Color(1, 1, 1, 1)
 		tutorial_button.text = "HIDE TUTORIALS"
